4052dd84a3e89fd230493db21ced760dc72038ef
Bushra Khan <bushra808x@gmail.com>
2023-07-31 18:59:26 +0530

    with password strength indicator

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
 const clipboard = document.getElementById('clipboard');
+const lengthErrorEl = document.getElementById('lengthError');
 
 const randomFunc = { //an object holding references to different functions
 	lower: getRandomLower,
@@ -29,11 +30,22 @@ clipboard.addEventListener('click', () => {
     document.execCommand('copy'); // Copy the selected text to the clipboard
     textarea.remove(); // Remove the textarea element from the document
     alert('Password copied to clipboard'); // Show an alert message to notify the user
+ 
   });
   
 
   generate.addEventListener('click', () => {
     const length = +lengthEl.value; // Get the user-selected password length as a number
+
+    if (length < 4 || length > 20) {
+      lengthErrorEl.textContent = "Please enter a valid length.";
+      lengthErrorEl.style.display = "block";
+      resultEl.innerText = "";
+      return;
+    } else {
+      lengthErrorEl.style.display = "none";
+    }
+  
     const hasLower = lowercaseEl.checked; // Check if the user selected lowercase letters
     const hasUpper = uppercaseEl.checked; // Check if the user selected uppercase letters
     const hasNumber = numbersEl.checked; // Check if the user selected numbers
@@ -41,7 +53,65 @@ clipboard.addEventListener('click', () => {
   
     // Call the generatePassword function with the user-selected criteria and update the result element
     resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+
+    const passwordStrength = getPasswordStrength(resultEl.innerText);
+    setStrengthIndicator(passwordStrength);
   });
+
+  function getPasswordStrength(password) {
+    const length = password.length;
+    const hasLower = /[a-z]/.test(password);
+    const hasUpper = /[A-Z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSymbol = /[^a-zA-Z\d]/.test(password);
+  
+    let strength = 0;
+  
+    // Assign points for each characteristic
+    if (length >= 10) {
+      strength += 2; // Good length
+    }
+    else if(length >= 8) {
+      strength += 1; // Good length
+    }
+  
+    if (hasLower || hasUpper) {
+      strength += 1; 
+    }
+  
+    if (hasNumber) {
+      strength += 1; // Numbers present
+    }
+  
+    if (hasSymbol) {
+      strength += 1; // Symbols present
+    }
+  
+    return strength;
+  }
+  
+  
+
+
+
+  function setStrengthIndicator(strength) {
+    const strengthBar = document.getElementById('strength-bar');
+    const strengthIndicator = document.querySelector('.strength-indicator');
+    const strengthTextValues = ['VERY WEAK', 'WEAK', 'OKAY', 'STRONG', 'VERY STRONG'];
+    const strengthClass = ['very-weak', 'weak', 'okay', 'strong', 'very-strong'];
+  
+    if (strength === 0) {
+      strengthIndicator.style.display = 'none';
+    } else {
+      strengthIndicator.style.display = 'block';
+      strengthBar.style.width = `${strength * 20}%`;
+      strengthBar.setAttribute('data-strength-text', strengthTextValues[strength - 1]);
+      strengthIndicator.className = 'strength-indicator ' + strengthClass[strength - 1];
+    }
+  }
+  
+  
+  
   
 
   function generatePassword(lower, upper, number, symbol, length) {
@@ -60,6 +130,9 @@ clipboard.addEventListener('click', () => {
       generatedPassword += randomFunc[funcName]();
     }
   
+    // const strengthIndicator = document.getElementById('strength-bar');
+    // strengthIndicator.innerText = getPasswordStrength(generatedPassword);
+    
     return generatedPassword;
   }
   
